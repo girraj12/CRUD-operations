2c05e7c0e53fb8fd05db28dd591bb7dcb457f48b
Girraj singhal <72375025+girraj12@users.noreply.github.com>
2024-06-14 19:43:08 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
-import {detail,getAmount,updateDetail,deleteDetail,getDetail} from '../controller/jobcontrol.js';
+import {user,detail,getAmount,updateDetail,deleteDetail,getDetail} from '../controller/jobcontrol.js';
 import express from 'express';
 const router = express.Router();
 
 router.post('/person',detail);
 router.get('/role/:job',getDetail);
 router.get('/amount/:amount', getAmount);
-router.put('/amountUpdate/:amount', updateDetail);
+router.patch('/amountUpdate/:amount', updateDetail);
 router.delete('/delete/:amount',deleteDetail);
-
+router.get('/search',user)
 export default router;
